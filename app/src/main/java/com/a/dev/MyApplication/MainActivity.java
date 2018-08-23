07e6add78c06b382d1271315dc56ad7df53a8873
Afan <ahmadmustofakamal94@gmail.com>
2018-08-23 16:59:05 +0700

    matreial searchbar

--- app/src/main/java/com/a/dev/MyApplication/MainActivity.java
+++ app/src/main/java/com/a/dev/MyApplication/MainActivity.java
@@ -1,14 +1,18 @@
 package com.a.dev.MyApplication;
 
 import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
@@ -17,9 +21,15 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private MenuItem action_search;
     private FrameLayout fragment_container;
+    private MaterialSearchView searchView;
     private FloatingActionButton fabExpand;
     private FloatingActionButton fabCollaps;
     private FloatingActionButton fabAdd;
@@ -30,11 +40,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
 
         fragment_container = findViewById(R.id.fragment_container);
+        searchView = findViewById(R.id.searchView);
+
         fabExpand = findViewById(R.id.fabExpand);
         fabCollaps = findViewById(R.id.fabCollaps);
         fabAdd = findViewById(R.id.fabAdd);
@@ -45,6 +57,35 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         switchToListMarkerFragmen();
+
+        searchView.setCursorDrawable(R.drawable.custom_cursor);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+                searchView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                searchView.setVisibility(View.GONE);
+            }
+        });
+        searchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
 
         fabExpand.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,10 +137,12 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.tab_MapLocation:
                     switchToListMarkerFragmen();
                     fabExpand.setVisibility(View.VISIBLE);
+                    action_search.setVisible(true);
                     return true;
                 case R.id.tab_ListLocation:
                     switchToListDataFragment();
                     fabExpand.setVisibility(View.VISIBLE);
+                    action_search.setVisible(true);
                     return true;
                 case R.id.tab_Info:
                     switchToInfoFragmen();
@@ -109,6 +152,12 @@ public class MainActivity extends AppCompatActivity {
                     fabCollaps.setVisibility(View.GONE);
                     fabAdd.setVisibility(View.INVISIBLE);
                     fabRefresh.setVisibility(View.INVISIBLE);
+
+                    action_search.setVisible(false);
+                    if (searchView.isSearchOpen()) {
+                        searchView.closeSearch();
+                    }
+
                     return true;
                 default:
                     switchToListMarkerFragmen();
@@ -134,6 +183,25 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction().replace(R.id.fragment_container, new InfoFragment()).commit();
         getSupportActionBar().setTitle(getResources().getString(R.string.tabInfo));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        action_search = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(action_search);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (searchView.isSearchOpen()) {
+            searchView.closeSearch();
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
